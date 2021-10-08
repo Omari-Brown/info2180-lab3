4112424cefb64ecfabc5d433d8e9f392b89e7856
Omari Brown <Omaribrown16@gmail.com>
2021-10-08 02:02:18 -0500

    Added marks that appear on board when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,37 @@
 "use strict";
+var mark = "X";
+
 window.onload = function() 
 {
     console.log("Loaded");
     const tiles = document.getElementById("board").children;
     console.log(tiles);
+    var tilestates = new Array(9);
     Array.from(tiles).forEach(function (tile)
     {
-        //console.log(tile);
-        tile.setAttribute("class", "square");
+        console.log(tile);
+        tile.classList.add("square");
+        tile.addEventListener("click", function ()
+        {
+            console.log("Setting properties when clicked if not already set");
+            //console.log("Outside if mark is now : " + mark);
+            if (!tile.classList.contains("X") && !tile.classList.contains("O"))
+            {
+                tile.classList.add(mark);
+                tile.textContent = mark;
+                if (mark == "X")
+                {
+                    mark = "O";
+                    //console.log("Inside if mark is now : " + mark);
+                } 
+                else
+                {
+                    mark = "X";
+                    //console.log(" Inside if mark is now : " + mark);
+                }            
+            } 
+        });
+
     });
-    
     
 }
